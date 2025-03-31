26554e86a5fbe6c6ee03ebb829666ed6dcef1cc0
Rowoon <dev.rowoon@gmail.com>
2025-03-31 14:27:27 +0900

    Initial Commit

---
+++ FastSwiftBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastSwiftBasic
+//
+//  Created by 이로운 on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastSwiftBasic/FastSwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSwiftBasicApp.swift
+//  FastSwiftBasic
+//
+//  Created by 이로운 on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct FastSwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
