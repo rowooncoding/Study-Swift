9231f432d1b817411b653356a92f01335de3d577
Rowoon <dev.rowoon@gmail.com>
2025-04-01 16:30:44 +0900

    학습을 위한 코드 수정

--- FastSwiftBasic/Diff.swift
+++ FastSwiftBasic/Diff.swift
@@ -10,18 +10,19 @@ import SwiftUI
 struct Diff: View {
     
     let myCar = Car(name: "밴츠", owner: "엄마")
-    let myKar = Kar(name: "아우디", owner: "로운")
+    @ObservedObject var myKar = Kar(name: "아우디", owner: "로운")
     
     var body: some View {
         VStack {
             Text("\(myCar.name)의 주인은 \(myCar.owner)")
             
             Button {
-                let broCar = myKar
+                var broCar = myCar
                 broCar.name = "bmw"
                 broCar.owner = "친구"
                 
-                myKar.sayHi()
+                myCar.sayHi()
+                broCar.sayHi()
             } label: {
                 Text("시동")
             }
@@ -39,8 +40,8 @@ struct Car {
     }
 }
 
-class Kar {
-    var name: String
+class Kar: ObservableObject {
+    @Published var name: String
     var owner: String
     
     init(name: String, owner: String) {
